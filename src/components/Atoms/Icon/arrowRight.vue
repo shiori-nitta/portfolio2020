ab442a606e576159c7dd02eecd111ce69cd4b219
nittashiori <nitta@impv.co.jp>
2020-12-18 09:41:12 +0900

    外側のdivを外してiconクラスをsvgに付与

--- src/components/Atoms/Icon/arrowRight.vue
+++ src/components/Atoms/Icon/arrowRight.vue
@@ -1,19 +1,9 @@
 <template>
-  <div class="icon">
-    <svg viewBox="0 0 24 24" fill="currentColor">
-      <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M18.19 12.75H4v-1.5h14.19l-6.72-6.72 1.06-1.06L21.06 12l-8.53 8.53-1.06-1.06 6.72-6.72z"
-      />
-    </svg>
-  </div>
+  <svg viewBox="0 0 24 24" fill="currentColor" class="icon">
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M18.19 12.75H4v-1.5h14.19l-6.72-6.72 1.06-1.06L21.06 12l-8.53 8.53-1.06-1.06 6.72-6.72z"
+    />
+  </svg>
 </template>
-
-<style lang="scss" scoped>
-.icon {
-  width: 24px;
-  height: 24px;
-  color: $text-color;
-}
-</style>
