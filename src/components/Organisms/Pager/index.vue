11a655f0d1cf19e61f9903ba74a22480e3fa2f00
nittashiori <nitta@impv.co.jp>
2020-12-21 11:56:03 +0900

    border削除

--- src/components/Organisms/Pager/index.vue
+++ src/components/Organisms/Pager/index.vue
@@ -11,10 +11,8 @@
   flex-wrap: wrap;
   justify-content: space-between;
   align-items: flex-start;
-  border: 1px solid red;
   > * {
     width: calc((100% - 40px) / 2);
-    border: 1px solid #000;
     @include media(md, max) {
       width: calc((100% - 20px) / 2);
     }
