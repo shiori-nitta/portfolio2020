0dd26e34b7214c77c7799e61630c88c12f2be40a
nittashiori <nitta@impv.co.jp>
2020-12-23 12:31:04 +0900

    トップで記事取得してたのをstoreに移動させて登録

---
+++ src/store/index.js
@@ -0,0 +1,31 @@
+import client from '~/plugins/contentful'
+
+export const state = () => ({
+  posts: [],
+})
+
+export const getters = {
+  posts(state) {
+    return state.posts
+  },
+}
+
+export const mutations = {
+  setPosts(state, payload) {
+    state.posts = payload
+  },
+}
+
+export const actions = {
+  async getPosts({ commit }) {
+    await client
+      .getEntries({
+        content_type: process.env.CTF_BLOG_POST_TYPE_ID,
+        order: '-sys.createdAt',
+      })
+      .then((res) => {
+        commit('setPosts', res.items)
+      })
+      .catch(console.error)
+  },
+}
