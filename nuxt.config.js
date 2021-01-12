ebe6fbf9a5ff4f58123b162229ac33a36c75d553
nittashiori <nitta@impv.co.jp>
2021-01-12 18:11:19 +0900

    vue-lazyloadアンインストール

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,11 +82,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    '~/plugins/adobe-fonts',
-    '~/plugins/contentful',
-    { src: '~/plugins/vue-lazyload.js', ssr: false },
-  ],
+  plugins: ['~/plugins/adobe-fonts', '~/plugins/contentful'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
